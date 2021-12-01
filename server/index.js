b9e442f349afcec8b8cadc537a005eb8b17ecbff
brygard14 <bryana.gardner@gmail.com>
2021-12-01 10:21:35 -0700

    initial code

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors =  require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('client'))
+
+app.get('/', () => {
+    resizeBy.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4004
+
+app.listen(port, () => console.log(`server running on ${port}!`))
